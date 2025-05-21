cd3eb4445a21a13911f7c0a981e589b5aaddfa43
zhaju <juleoan123@gmail.com>
2025-05-20 21:43:53 -0400

    added tag

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-G9BW44VG41"
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
