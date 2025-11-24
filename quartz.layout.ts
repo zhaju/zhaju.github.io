6d9110eeb525e43b53d318e83d63ef2e6264327b
zhaju <juleoan123@gmail.com>
2025-11-24 13:54:29 -0500

    cleaned up sidebar

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,9 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
